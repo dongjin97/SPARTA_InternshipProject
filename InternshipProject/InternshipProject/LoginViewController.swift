9f7b8dbe6595ae9d3e15c3f3533dac3d1a644aee
wondongjin <82082770+dongjin97@users.noreply.github.com>
2024-08-07 14:07:08 +0900

    [Feat] ios 최소버전 변경, 키보드가 시작하기 버튼 가림현상 수정

--- InternshipProject/InternshipProject/LoginViewController.swift
+++ InternshipProject/InternshipProject/LoginViewController.swift
@@ -53,13 +53,13 @@ extension LoginViewController {
     private func setAutoLayout() {
         emailTextField.snp.makeConstraints { make in
             make.centerY.equalToSuperview()
-            make.left.right.equalToSuperview().inset(20)
+            make.left.right.equalToSuperview().inset(15)
         }
         
         startButton.snp.makeConstraints { make in
-            make.top.greaterThanOrEqualTo(emailTextField.snp.bottom)
-            make.left.right.equalToSuperview().inset(20)
-            make.bottom.equalToSuperview().offset(-100)
+            make.top.greaterThanOrEqualTo(emailTextField.snp.bottom).offset(15)
+            make.left.right.equalToSuperview().inset(15)
+            make.bottom.greaterThanOrEqualTo(view.keyboardLayoutGuide.snp.top).offset(-50)
         }
     }
 }

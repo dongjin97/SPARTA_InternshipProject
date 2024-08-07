01a03edf62a45ac29a43cd42dfab75c0986542af
wondongjin <82082770+dongjin97@users.noreply.github.com>
2024-08-07 13:05:12 +0900

    [init] View 배경 설정

---
+++ InternshipProject/InternshipProject/Extension+UIViewController.swift
@@ -0,0 +1,16 @@
+//
+//  Extension+UIViewController.swift
+//  InternshipProject
+//
+//  Created by 원동진 on 8/7/24.
+//
+
+import UIKit
+
+// MARK: - Extension + UIViewController
+
+extension UIViewController {
+    func setBackgroundColor() {
+        self.view.backgroundColor = .white
+    }
+}
--- InternshipProject/InternshipProject/LoginViewController.swift
+++ InternshipProject/InternshipProject/LoginViewController.swift
@@ -5,15 +5,21 @@
 //  Created by 원동진 on 8/7/24.
 //
 
+// MARK: - import
+
 import UIKit
+import SnapKit
+
+// MARK: - LoginViewController
 
 class LoginViewController: UIViewController {
-
+    
+    // MARK: - LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setBackgroundColor()
     }
-
-
 }
 

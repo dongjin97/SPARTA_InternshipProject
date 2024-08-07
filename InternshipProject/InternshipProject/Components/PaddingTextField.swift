a82010c587eeaecc104d676b3159f1b3026934fa
wondongjin <82082770+dongjin97@users.noreply.github.com>
2024-08-07 13:55:39 +0900

    [Feat] #1 -PaddingTextField 추가

---
+++ InternshipProject/InternshipProject/Components/PaddingTextField.swift
@@ -0,0 +1,36 @@
+//
+//  PaddingTextField.swift
+//  InternshipProject
+//
+//  Created by 원동진 on 8/7/24.
+//
+
+import UIKit
+
+// MARK: - PaddingTextField
+
+class PaddingTextField : UITextField {
+    var textPadding = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+    convenience init(textPadding: UIEdgeInsets) {
+        self.init()
+        self.textPadding = textPadding
+    }
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        let rect = super.textRect(forBounds: bounds)
+        return rect.inset(by: textPadding)
+    }
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        let rect = super.editingRect(forBounds: bounds)
+        return rect.inset(by: textPadding)
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.layer.cornerRadius = 10
+        self.layer.borderWidth = 1
+        self.layer.masksToBounds = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}

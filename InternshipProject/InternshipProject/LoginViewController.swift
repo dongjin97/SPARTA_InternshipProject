9707a8d1c8a62fed53a91a3fd896e06032ace434
wondongjin <82082770+dongjin97@users.noreply.github.com>
2024-08-07 13:23:14 +0900

    [Feat] #1 - 이메일 입력 TextField, 시작하기 버튼 추가

--- InternshipProject/InternshipProject/LoginViewController.swift
+++ InternshipProject/InternshipProject/LoginViewController.swift
@@ -14,12 +14,54 @@ import SnapKit
 
 class LoginViewController: UIViewController {
     
+    // MARK: - SubViews
+    
+    private lazy var emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "이메일을 입력하세요."
+        textField.layer.cornerRadius = 10
+        textField.layer.masksToBounds = true
+        textField.layer.borderWidth = 1
+        
+        return textField
+    }()
+    
+    private lazy var startButton: UIButton = {
+       let button = UIButton()
+        button.setTitle("시작하기", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        
+        return button
+    }()
+    
     // MARK: - LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setBackgroundColor()
+        setAddSubViews()
+        setAutoLayout()
     }
 }
 
+// MARK: - Layout Helpers
+
+extension LoginViewController {
+    private func setAddSubViews() {
+        view.addSubViews([emailTextField,startButton])
+    }
+    
+    private func setAutoLayout() {
+        emailTextField.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.left.right.equalToSuperview().inset(20)
+        }
+        
+        startButton.snp.makeConstraints { make in
+            make.top.greaterThanOrEqualTo(emailTextField.snp.bottom)
+            make.left.right.equalToSuperview().inset(20)
+            make.bottom.equalToSuperview().offset(-100)
+        }
+    }
+}
